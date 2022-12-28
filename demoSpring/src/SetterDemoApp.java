0872964666cb0e04ffe0c7ed6a61c293686d0d7a
Abhinav Gupta <201851003@iiitvadodara.ac.in>
2022-12-29 03:25:13 +0530

    Setter Injection || Reading from properties files Video 42

--- demoSpring/src/SetterDemoApp.java
+++ demoSpring/src/SetterDemoApp.java
@@ -1,5 +1,7 @@
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import javax.swing.*;
+
 public class SetterDemoApp {
     public static void main(String [] args){
         //load the spring configuration file
@@ -13,6 +15,11 @@ public class SetterDemoApp {
         System.out.println(theCoach.getDailyFortune());
         System.out.println(theCoach.getEmailAddress());
         System.out.println(theCoach.getTeam());
+
+        // Injecting values from properties file
+//        1. Create Properties file
+//        2. Load Properties files in Spring config file
+//        3. Reference values from Properties file
 
         //close the context
         context.close();
