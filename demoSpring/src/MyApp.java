0f8f00bf5b71e83be67b19496a664d967e15a590
Abhinav Gupta <201851003@iiitvadodara.ac.in>
2022-12-28 13:56:14 +0530

    Spring IOC Documentation

--- demoSpring/src/MyApp.java
+++ demoSpring/src/MyApp.java
@@ -7,7 +7,28 @@ public class MyApp {
 
 
         // Get Workput for the Gymnastics
-        Coach theCoahc1= new GymCoach();
-        System.out.println(theCoahc1.getDailyWorkout());
+        Coach theCoach1= new GymCoach();
+        System.out.println(theCoach1.getDailyWorkout());
+
+        Coach theCoach2 = new TrackCoach();
+        System.out.println(theCoach2.getDailyWorkout());
+
+        // Video 22: Spring Inversion of Control
+//        Spring Development Process
+//            1. Configre your Spring Beans
+//        applicationContext.xml
+//        <bean id="myCoach"
+//            class="demoSpring.BaseballCoach">
+//            </bean>
+
+//            2. Create a Spring Container
+//            ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+//            3. Retrieve Beans from Spring Container
+//              Coach theCoach = context.getBean("myCoach", Coach.class);
+
+
+
+
     }
 }
---
+++ demoSpring/src/TrackCoach.java
@@ -0,0 +1,7 @@
+public class TrackCoach implements Coach {
+
+    @Override
+    public String getDailyWorkout(){
+        return "RUN a hard 5k";
+    }
+}
